ceca60e276fb9c707450ee55550892c81415b77b
Jason Myers <myer0154@umn.edu>
2017-01-04 14:01:52 -0600

    Finish plot1.R; add plot1.png

--- Assignment 2 - Week 4/plot1.R
+++ Assignment 2 - Week 4/plot1.R
@@ -5,5 +5,13 @@ if(!file.exists("./summarySCC_PM25.rds")) {
   file.remove("./temp_data.zip")
 }
 
-SCC <- readRDS("Source_Classification_Code.rds")
+# read the source files
 pm25 <- readRDS("summarySCC_PM25.rds")
+
+# aggregate the data using Emissions as a function of year and apply the sum() function
+tot_emissions <- aggregate(Emissions ~ year, pm25, sum)
+
+# create a new png device, plot the data, then close the device
+png("plot1.png")
+barplot(tot_emissions$Emissions, names.arg = tot_emissions$y, xlab = "Year", ylab = "Emission (tons)", main = "Total PM2.5 emission from all sources")
+dev.off()
